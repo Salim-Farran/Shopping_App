784f28401dfede3a5ae78286fc3a4d9b77179e83
Salim Farran <salim.farran93@gmail.com>
2026-02-16 15:28:46 +0100

    Added some products images

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -5,36 +5,46 @@ const List<Product> allProducts = [
   Product(id: '1',
       title: 'Groovy shorts',
       price: 12,
-      image: ''
+      image: 'assets/shorts.png'
   ),
-  Product(id: '1',
-      title: 'Groovy shorts',
+  Product(id: '2',
+      title: 'Drum',
       price: 55,
-      image: ''
+      image: 'assets/drum.png'
   ),
-  Product(id: '1',
-      title: 'Groovy shorts',
-      price: 12,
-      image: ''
+  Product(id: '3',
+      title: 'Back pack',
+      price: 29,
+      image: 'assets/backpack.png'
   ),
-  Product(id: '1',
-      title: 'Groovy shorts',
-      price: 12,
-      image: ''
+  Product(id: '4',
+      title: 'Electric Guitar',
+      price: 35,
+      image: 'assets/guitar.png'
   ),
-  Product(id: '1',
-      title: 'Groovy shorts',
-      price: 12,
-      image: ''
+  Product(id: '5',
+      title: 'Jeans',
+      price: 47,
+      image: 'assets/jeans.png'
   ),
-  Product(id: '1',
-      title: 'Groovy shorts',
-      price: 12,
-      image: ''
+  Product(id: '6',
+      title: 'Karati',
+      price: 67,
+      image: 'assets/karati.png'
+  ),
+  Product(id: '7',
+      title: 'Skates',
+      price: 34,
+      image: 'assets/skates.png'
+  ),
+  Product(id: '8',
+      title: 'Suit case',
+      price: 25,
+      image: 'assets/suitcase.png'
   ),
 ];
 
-final productsProvide = Provider((ref) {
+final productsProvider = Provider((ref) {
   return allProducts;
 });
 
